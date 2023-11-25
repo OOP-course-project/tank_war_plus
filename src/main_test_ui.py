2a7ca0b3749237712270275693a6fca98318de0c
夜落随悠 <1264631168@qq.com>
2023-11-25 23:56:52 +0800

    Refactor button drawing code

--- src/main_test_ui.py
+++ src/main_test_ui.py
@@ -8,6 +8,7 @@ import game_mode
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+
 def draw_button(screen, button_type, color, x, y, width, height, text, radius=None):
     button_color = color
 
@@ -20,7 +21,9 @@ def draw_button(screen, button_type, color, x, y, width, height, text, radius=No
         if radius is None:
             radius = 10  # default radius
 
-        pygame.draw.rect(screen, button_color, (x, y, width, height), border_radius=radius)
+        pygame.draw.rect(
+            screen, button_color, (x, y, width, height), border_radius=radius
+        )
     elif button_type == "rectangle":
         pygame.draw.rect(screen, button_color, (x, y, width, height))
         pygame.draw.rect(screen, (0, 0, 0), (x, y, width, height), 1)
@@ -52,13 +55,42 @@ def main():
         button_height = 100
 
         # Draw Single Game button (rounded rectangle, red)
-        draw_button(screen, "rounded_rectangle", (220, 20, 20), 350, 120, button_width, button_height, "Single Game", radius=10)
+        draw_button(
+            screen,
+            "rounded_rectangle",
+            (220, 20, 20),
+            350,
+            120,
+            button_width,
+            button_height,
+            "Single Game",
+            radius=10,
+        )
 
         # Draw Double Game button (rounded rectangle, green)
-        draw_button(screen, "rounded_rectangle", (30, 220, 30), 350, 300, button_width, button_height, "Double Game", radius=10)
+        draw_button(
+            screen,
+            "rounded_rectangle",
+            (30, 220, 30),
+            350,
+            300,
+            button_width,
+            button_height,
+            "Double Game",
+            radius=10,
+        )
 
         # Draw Settings button (rectangle, tan)
-        draw_button(screen, "rectangle", (210, 180, 140), 0, 400, button_width, button_height, "Settings")
+        draw_button(
+            screen,
+            "rectangle",
+            (210, 180, 140),
+            0,
+            400,
+            button_width,
+            button_height,
+            "Settings",
+        )
 
         pygame.display.flip()
 
@@ -67,30 +99,84 @@ def main():
                 running = False
             elif event.type == pygame.MOUSEMOTION:
                 mouse_pos = pygame.mouse.get_pos()
-                if 350 <= mouse_pos[0] <= 350 + button_width and 120 <= mouse_pos[1] <= 120 + button_height:
-                    button_color = tuple(min(c + 30, 255) for c in (255, 0, 0))  # Lighten the color
-                elif 350 <= mouse_pos[0] <= 350 + button_width and 300 <= mouse_pos[1] <= 300 + button_height:
-                    button_color = tuple(min(c + 30, 255) for c in (0, 255, 0))  # Lighten the color
-                elif 0 <= mouse_pos[0] <= button_width and 400 <= mouse_pos[1] <= 400 + button_height:
-                    button_color = tuple(min(c + 30, 255) for c in (210, 180, 140))  # Lighten the color
+                if (
+                    350 <= mouse_pos[0] <= 350 + button_width
+                    and 120 <= mouse_pos[1] <= 120 + button_height
+                ):
+                    button_color = tuple(
+                        min(c + 30, 255) for c in (255, 0, 0)
+                    )  # Lighten the color
+                elif (
+                    350 <= mouse_pos[0] <= 350 + button_width
+                    and 300 <= mouse_pos[1] <= 300 + button_height
+                ):
+                    button_color = tuple(
+                        min(c + 30, 255) for c in (0, 255, 0)
+                    )  # Lighten the color
+                elif (
+                    0 <= mouse_pos[0] <= button_width
+                    and 400 <= mouse_pos[1] <= 400 + button_height
+                ):
+                    button_color = tuple(
+                        min(c + 30, 255) for c in (210, 180, 140)
+                    )  # Lighten the color
                 else:
                     button_color = (255, 0, 0)
 
-                draw_button(screen, "rounded_rectangle", button_color, 350, 120, button_width, button_height, "Single Game", radius=10)
-                draw_button(screen, "rounded_rectangle", button_color, 350, 300, button_width, button_height, "Double Game", radius=10)
-                draw_button(screen, "rectangle", button_color, 0, 400, button_width, button_height, "Settings")
+                draw_button(
+                    screen,
+                    "rounded_rectangle",
+                    button_color,
+                    350,
+                    120,
+                    button_width,
+                    button_height,
+                    "Single Game",
+                    radius=10,
+                )
+                draw_button(
+                    screen,
+                    "rounded_rectangle",
+                    button_color,
+                    350,
+                    300,
+                    button_width,
+                    button_height,
+                    "Double Game",
+                    radius=10,
+                )
+                draw_button(
+                    screen,
+                    "rectangle",
+                    button_color,
+                    0,
+                    400,
+                    button_width,
+                    button_height,
+                    "Settings",
+                )
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                if 350 <= mouse_pos[0] <= 350 + button_width and 120 <= mouse_pos[1] <= 120 + button_height:
+                if (
+                    350 <= mouse_pos[0] <= 350 + button_width
+                    and 120 <= mouse_pos[1] <= 120 + button_height
+                ):
                     game_mode.game_mode(screen)
-                elif 350 <= mouse_pos[0] <= 350 + button_width and 300 <= mouse_pos[1] <= 300 + button_height:
+                elif (
+                    350 <= mouse_pos[0] <= 350 + button_width
+                    and 300 <= mouse_pos[1] <= 300 + button_height
+                ):
                     game_mode.game_mode(screen, double_players=True)
-                elif 0 <= mouse_pos[0] <= button_width and 400 <= mouse_pos[1] <= 400 + button_height:
+                elif (
+                    0 <= mouse_pos[0] <= button_width
+                    and 400 <= mouse_pos[1] <= 400 + button_height
+                ):
                     # Add settings functionality here
                     pass
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     try:
@@ -101,5 +187,3 @@ if __name__ == "__main__":
         traceback.print_exc()
         pygame.quit()
         input()
-
-
